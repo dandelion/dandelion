42f95db33bb816b6aa93a736da865cc3cb437960
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-10-29 12:02:35 +0100

    New Spring way to render cached web resources Updated the AssetController, allowing to render cached web resource instead of the configured concrete implementation of the AssetServlet The Spring dependency is now based on a range, starting from the 3.0.0.RELEASE (@ResponseBody) Note that:  * the controller must be configured in end-user applications using: <import resource="classpath:dandelion.xml" />  * No need to add the import directive in almost all Dandelion samples since the base package scanned in the Spring config is "com.github.dandelion", which is most of talready scanned

--- dandelion-extras/dandelion-spring3/src/main/java/com/github/dandelion/extras/spring3/web/AssetController.java
+++ dandelion-extras/dandelion-spring3/src/main/java/com/github/dandelion/extras/spring3/web/AssetController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,38 +17,49 @@ import com.github.dandelion.core.asset.AssetType;
 import com.github.dandelion.core.asset.cache.AssetsCacheSystem;
 import com.github.dandelion.core.asset.web.AssetServlet;
 
+/**
+ * <p>
+ * Controller used to render cached web resources instead of a concrete
+ * implementation of {@link AssetServlet}.
+ * <p>
+ * Note that client applications must import the corresponding Spring config
+ * file in order for this controller to be scanned. For example using:
+ * 
+ * <pre>
+ * <code>&lt;import resource="classpath:dandelion.xml" /&gt;</code>
+ * </pre>
+ * 
+ * @since 0.3.0
+ */
 @Controller
-@RequestMapping(value = AssetServlet.DANDELION_ASSETS_URL)
+@RequestMapping(value = "/dandelion-assets", method = RequestMethod.GET)
 public class AssetController {
 
 	// Logger
-    private static final Logger LOG = LoggerFactory.getLogger(AssetController.class);
+	private static final Logger LOG = LoggerFactory.getLogger(AssetController.class);
 
-    @RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody String renderJs(HttpServletResponse response, HttpServletRequest request) throws IOException {
+	@RequestMapping(value = "/{assetKey}")
+	public @ResponseBody
+	String renderAsset(@PathVariable(value = "assetKey") String assetKey, HttpServletResponse response,
+			HttpServletRequest request) throws IOException {
 
-    	
 		String cacheKey = AssetsCacheSystem.getCacheKeyFromRequest(request);
 
-//        if (isDevModeEnabled() && !AssetsCacheSystem.checkCacheKey(cacheKey)) {
-//        	throw new ServletException("The Dandelion assets should have been generated!");
-//        }
+		AssetType resourceType = AssetType.typeOfAsset(cacheKey);
+		if (resourceType == null) {
+			LOG.debug("Unknown asset type from key {}", cacheKey);
+			return null;
+		}
 
-        AssetType resourceType = AssetType.typeOfAsset(cacheKey);
-        if (resourceType == null) {
-        	LOG.debug("Unknown asset type from key {}", cacheKey);
-//        	return;
-        }
+		String fileContent = AssetsCacheSystem.getCacheContent(cacheKey);
+		if (fileContent == null) {
+			LOG.debug("Missing content from key {}", cacheKey);
+			return null;
+		}
 
-        String fileContent = AssetsCacheSystem.getCacheContent(cacheKey);
-        if(fileContent == null) {
-        	LOG.debug("Missing content from key {}", cacheKey);
-//            return;
-        }
-        
-        return fileContent;
-//        response.setContentType(resourceType.getContentType());
-//        response.setHeader("Cache-Control", "no-cache");
-//        response.getWriter().write(fileContent);
+		response.setContentType(resourceType.getContentType());
+		response.setHeader("Cache-Control", "no-cache");
+
+		return fileContent;
 	}
 }
