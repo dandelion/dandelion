1bc3437b48e60d836980a417e065e5707c816b6f
Thibault Duchateau <thibault.duchateau@gmail.com>
2014-02-09 18:43:14 +0100

    Added support for relative paths in CSS files when loading asset from a Webjar

--- dandelion-extras/dandelion-webjars/src/main/java/com/github/dandelion/extras/webjars/asset/wrapper/WebjarsLocationWrapper.java
+++ dandelion-extras/dandelion-webjars/src/main/java/com/github/dandelion/extras/webjars/asset/wrapper/WebjarsLocationWrapper.java
@@ -30,16 +30,27 @@
 
 package com.github.dandelion.extras.webjars.asset.wrapper;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.webjars.WebJarAssetLocator;
 
+import com.github.dandelion.core.DandelionException;
 import com.github.dandelion.core.asset.Asset;
+import com.github.dandelion.core.asset.AssetType;
+import com.github.dandelion.core.asset.wrapper.AssetLocationWrapperError;
 import com.github.dandelion.core.asset.wrapper.impl.CacheableLocationWrapper;
 import com.github.dandelion.core.asset.wrapper.spi.AssetLocationWrapper;
 import com.github.dandelion.core.utils.ResourceUtils;
+import com.github.dandelion.core.utils.StringUtils;
 
 /**
  * <p>
@@ -54,7 +65,10 @@ import com.github.dandelion.core.utils.ResourceUtils;
  * @since 0.10.0
  */
 public class WebjarsLocationWrapper extends CacheableLocationWrapper {
+	
 	private static WebJarAssetLocator locator = new WebJarAssetLocator();
+	private static Pattern pattern = Pattern.compile("url\\((.*)\\)", Pattern.CASE_INSENSITIVE);
+	private HttpServletRequest request;
 
 	public WebjarsLocationWrapper() {
 		this.active = true;
@@ -73,7 +87,75 @@ public class WebjarsLocationWrapper extends CacheableLocationWrapper {
 	 */
 	@Override
 	protected String getContent(Asset asset, String location, Map<String, Object> parameters, HttpServletRequest request) {
+		this.request = request;
+
 		String webjarsLocation = locator.getFullPath(location);
-		return ResourceUtils.getFileContentFromClasspath(webjarsLocation, false);
+
+		String content = ResourceUtils.getFileContentFromClasspath(webjarsLocation, false);
+
+		if (asset.getType().equals(AssetType.css) && content.contains("..")) {
+			content = processRelativePaths(content, webjarsLocation.substring(webjarsLocation.indexOf("webjars")));
+		}
+		return content;
+	}
+
+	public String processRelativePaths(String content, String url) {
+
+		StringWriter sw = new StringWriter();
+		BufferedReader reader = new BufferedReader(new StringReader(content));
+		BufferedWriter writer = new BufferedWriter(sw);
+
+		try {
+			String line = null;
+
+			while ((line = reader.readLine()) != null) {
+
+				Matcher matcher = pattern.matcher(line);
+				while (matcher.find()) {
+
+					int lvl = StringUtils.countMatches(line, "..");
+
+					if (lvl > 0) {
+						String parentPath = null;
+						int c = lvl + 1;
+						for (int i = url.length() - 1; i >= 0; i--) {
+							if (c > 0 && url.charAt(i) == '/') {
+								parentPath = url.substring(0, i + 1);
+								c--;
+							}
+
+							if (c == 0) {
+								break;
+							}
+						}
+						line = line.replaceAll("(\\.\\./)+", request.getContextPath() + "/" + parentPath);
+					}
+				}
+				writer.write(line + '\n');
+			}
+		}
+		catch (IOException e) {
+			throw new DandelionException(AssetLocationWrapperError.ASSET_RELATIVE_PATHS);
+		}
+		finally {
+			try {
+				if (reader != null)
+					reader.close();
+			}
+			catch (IOException e) {
+				// Should never happen
+				throw new DandelionException(AssetLocationWrapperError.ASSET_RELATIVE_PATHS);
+			}
+			try {
+				if (writer != null)
+					writer.close();
+			}
+			catch (IOException e) {
+				// Should never happen
+				throw new DandelionException(AssetLocationWrapperError.ASSET_RELATIVE_PATHS);
+			}
+		}
+
+		return sw.getBuffer().toString();
 	}
 }
